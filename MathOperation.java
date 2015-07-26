74c61928d6cf82e9f50428878686c0f536081550
farhanmia <fmeah16@gmail.com>
2015-07-26 12:05:50 -0500

    My First Commit
    My First Commit

---
+++ MathOperation.java
@@ -0,0 +1,34 @@
+package com.digitek.mathoperation;
+
+public class MathOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		subtractionnumbers();
+		additionnumbers();
+	}
+
+
+
+	public static void subtractionnumbers() {
+		
+		int number1 = 20;
+		int number2 = 10;
+		
+		int subtraction = number1-number2;
+		
+		System.out.println(subtraction);
+	}
+	
+	public static void additionnumbers() {
+		
+		int number1 = 25;
+		int number2 = 25;
+		
+		int addition = number1+number2;
+		
+		System.out.println(addition);
+		
+	}
+	
+}
